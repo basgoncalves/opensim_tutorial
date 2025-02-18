adaeb4faf67ff8e81936728845db0a45c1f96df6
Vasilisa98 <vasilisa.djalovic@gmail.com>
2025-02-18 13:34:20 +0100

    Revert "Calculating FS"
    This reverts commit 98401362839dbe133f292e47ff6cbff6d654681f.

--- tutorials/repeated_sprinting/pipeline.py
+++ tutorials/repeated_sprinting/pipeline.py
@@ -1,20 +1,9 @@
 import os 
 import subprocess
-import msk_modelling_python as msk
+# import msk_modelling_python as msk
 import sys
 import opensim as osim
 
-file_path = r"C:\opensim_tutorial\tutorials\repeated_sprinting\Simulations\P013\trial3_r1\processed_emg_signals.csv"
-emg_data = msk.bops.pd.read_csv(file_path)
-
-fs = int(1/(emg_data['Time'][1] - emg_data['Time'][0]))
-
-time = emg_data
-
-
-import pdb; pdb.set_trace()
-
-exit()
 
 try:
     model = osim.Model()
@@ -25,8 +14,8 @@ except Exception as e:
 
 
 current_path = os.path.dirname(__file__)
-#print(current_path)
-#print(sys.executable)
+print(current_path)
+print(sys.executable)
 
 try:
     import msk_modelling_python as msk
@@ -50,21 +39,19 @@ else:
 #Define the path to the main XML setup file
 current_path = os.path.dirname(__file__)
 xml_setup_file = os.path.normpath(os.path.join(current_path, "Simulations", "P013", "trial3_r1", "ceinms", "calibrationSetup.xml"))
-#xml_setup_file = os.path.normpath(os.path.join(current_path, "Simulations", "009", "pre", "ceinms", "calibration", "calibrationSetup.xml"))
-#print(current_path)
-#print(xml_setup_file)
+print(current_path)
+print(xml_setup_file)
 
 # Construct the command
 command = " ".join([ceinms_install_path + "\CEINMScalibrate.exe -S", xml_setup_file])
 
-print('Calibration command:', command)
-exit()
+print(command)
 
-# if os.path.exists(xml_setup_file):
-#     print("XML file exists:", xml_setup_file)
-# else:
-#     print("ERROR: XML file doesn't exist:", xml_setup_file)
-#     sys.exit(1)
+if os.path.exists(xml_setup_file):
+    print("XML file exists:", xml_setup_file)
+else:
+    print("ERROR: XML file doesn't exist:", xml_setup_file)
+    sys.exit(1)
 
 # Run the command
 try:
@@ -76,10 +63,10 @@ except subprocess.CalledProcessError as e:
 
 result = subprocess.run(command, capture_output=True, text=True, check=True)
 
-# try: 
-#     print('Run the CEINMS executable')
-#      os.system(command)
-# except Exception as e:
-#     print('ERROR:', e)
-#     sys.exit(1)
+try: 
+    print('Run the CEINMS executable')
+    # os.system(command)
+except Exception as e:
+    print('ERROR:', e)
+    sys.exit(1)
     
