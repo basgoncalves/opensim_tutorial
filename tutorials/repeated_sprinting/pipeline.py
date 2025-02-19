f1526ed8f8b078d0be932785571d47a1cc38b7c1
basgoncalves <basilio.goncalves7@gmail.com>
2025-02-19 11:32:06 +0100

    class pipeline and add ceinms command txt

--- tutorials/repeated_sprinting/pipeline.py
+++ tutorials/repeated_sprinting/pipeline.py
@@ -23,69 +23,58 @@ class Project:
 
         return f'name {name}\n datacolumns {num_cols}\n datarows {num_rows}\n range {df_range} \n endheader'
     
+    def csv_to_mot(self, csv_path):
+        
+        emg_data = msk.bops.pd.read_csv(file_path)
+
+        fs = int(1/(emg_data['Time'][1] - emg_data['Time'][0]))
+
+        time = emg_data['Time']
+
+        # start time from new time point
+        start_time = 1.539
+        end_time = time.iloc[-1] - time.iloc[0] + start_time
+
+        num_samples = len(emg_data)
+        #num_samples = int((end_time - start_time) / (1/fs))
+        new_time = np.linspace(start_time, end_time, num_samples)
+
+        emg_data['Time'] = new_time
+
+        # Define a new file path 
+        new_file_path = os.path.join(P.simulations, r'P013\trial3_r1\processed_emg_signals_updated.csv')
+
+        # Save the modified DataFrame
+        emg_data.to_csv(new_file_path, index=False)  # index=False prevents adding an extra index column
+
+        # save to mot
+        header = P.header_mot(emg_data, "processed_emg_signals")
+
+        mot_path = new_file_path.replace('.csv','.mot')
+        with open(mot_path, 'w') as f:
+            f.write(header + '\n')  
+            # print column names 
+            f.write('\t'.join(map(str, emg_data.columns)) + '\n')
+            for index, row in emg_data.iterrows():
+                f.write('\t'.join(map(str, row.values)) + '\n')  # C
+
+    def check_osim_model(self):
+        try:
+            model = osim.Model()
+            print("OpenSim model created successfully!")
+
+        except Exception as e:
+            print(e)
+
+    
 P = Project(os.path.dirname(__file__))
 file_path = os.path.join(P.simulations, r'P013\trial3_r1\processed_emg_signals.csv')
-
-emg_data = msk.bops.pd.read_csv(file_path)
-
-fs = int(1/(emg_data['Time'][1] - emg_data['Time'][0]))
-
-time = emg_data['Time']
-
-# start time from new time point
-start_time = 1.539
-end_time = time.iloc[-1] - time.iloc[0] + start_time
-
-num_samples = len(emg_data)
-#num_samples = int((end_time - start_time) / (1/fs))
-new_time = np.linspace(start_time, end_time, num_samples)
-
-emg_data['Time'] = new_time
-
-# Define a new file path 
-new_file_path = os.path.join(P.simulations, r'P013\trial3_r1\processed_emg_signals_updated.csv')
-
-# Save the modified DataFrame
-emg_data.to_csv(new_file_path, index=False)  # index=False prevents adding an extra index column
-
-
-# save to mot
-header = P.header_mot(emg_data, "processed_emg_signals")
-
-
-
-mot_path = new_file_path.replace('.csv','.mot')
-with open(mot_path, 'w') as f:
-    f.write(header + '\n')  
-    # print column names 
-    f.write('\t'.join(map(str, emg_data.columns)) + '\n')
-    for index, row in emg_data.iterrows():
-        f.write('\t'.join(map(str, row.values)) + '\n')  # C
-
-
-import pdb; pdb.set_trace()
-
-exit()
-
-try:
-    model = osim.Model()
-    print("OpenSim model created successfully!")
-
-except Exception as e:
-    print(e)
+# P.csv_to_mot(file_path)
 
 
 current_path = os.path.dirname(__file__)
 print(current_path)
 print(sys.executable)
-
-try:
-    import msk_modelling_python as msk
-    msk.bops.test_bops()
-
-    print('MSk loaded properly')
-except Exception as e:
-    print('ERROR:', e)
 
 # run CEINMS
 ceinms_install_path = msk.__path__[0] + '\src' + '\ceinms2' + '\src'
@@ -109,6 +98,7 @@ command = " ".join([ceinms_install_path + "\CEINMScalibrate.exe -S", xml_setup_f
 
 print(command)
 
+exit()
 if os.path.exists(xml_setup_file):
     print("XML file exists:", xml_setup_file)
 else:
