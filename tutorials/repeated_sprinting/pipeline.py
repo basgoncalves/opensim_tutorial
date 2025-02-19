3ecbe46aada5a77b1271c530dcc7b0b88da0e8cc
basgoncalves <basilio.goncalves7@gmail.com>
2025-02-19 11:43:21 +0100

    minor

--- tutorials/repeated_sprinting/pipeline.py
+++ tutorials/repeated_sprinting/pipeline.py
@@ -11,7 +11,6 @@ class Project:
         self.simulations = os.path.join(path, 'Simulations')
         self.pipeline = os.path.dirname(__file__)
     
-
     def header_mot(self,df,name):
 
         num_rows = len(df)
@@ -71,7 +70,6 @@ P = Project(os.path.dirname(__file__))
 file_path = os.path.join(P.simulations, r'P013\trial3_r1\processed_emg_signals.csv')
 # P.csv_to_mot(file_path)
 
-
 current_path = os.path.dirname(__file__)
 print(current_path)
 print(sys.executable)
@@ -90,6 +88,7 @@ else:
 #Define the path to the main XML setup file
 current_path = os.path.dirname(__file__)
 xml_setup_file = os.path.normpath(os.path.join(current_path, "Simulations", "P013", "trial3_r1", "ceinms", "calibrationSetup.xml"))
+xml_setup_file = os.path.normpath(os.path.join(current_path, "Simulations", "009", "pre", "ceinms","calibration" ,"calibrationSetup.xml"))
 print(current_path)
 print(xml_setup_file)
 
@@ -98,7 +97,7 @@ command = " ".join([ceinms_install_path + "\CEINMScalibrate.exe -S", xml_setup_f
 
 print(command)
 
-exit()
+
 if os.path.exists(xml_setup_file):
     print("XML file exists:", xml_setup_file)
 else:
