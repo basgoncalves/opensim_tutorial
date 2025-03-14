040514e7dce0d864a35a9b8173bc679b57b5363a
basgoncalves <basilio.goncalves7@gmail.com>
2025-03-14 11:18:22 +0100

    Remove obsolete test files and add new resources for meshLab and repeated sprinting tutorials

--- tutorials/repeated_sprinting/code/ceinms.py
+++ tutorials/repeated_sprinting/code/ceinms.py
@@ -15,8 +15,8 @@ def run_calibration(xml_setup_file=None, ):
         ceinms_install_path = os.path.join(msk.__path__[0], 'src', 'ceinms2', 'src')
         command = " ".join([ceinms_install_path + "\CEINMScalibrate.exe -S", xml_setup_file])
         print(command)
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
-        
+        # result = subprocess.run(command, capture_output=True, text=True, check=True)
+        result = None
         return result
     except Exception as e:
         print(e)
@@ -24,5 +24,5 @@ def run_calibration(xml_setup_file=None, ):
     
 if __name__ == '__main__':
     xml_setup_file = os.path.join(os.path.dirname(code_path), 'Simulations', '009_simplified', 'run_baseline', 'ceinms', 'calibration_setup.xml')
-    xml_setup_file = r'"C:\Git\opensim_tutorial\tutorials\repeated_sprinting\Simulations\009\pre\ceinms\calibration\calibrationSetup.xml"'
-    run_calibration()
+    xml_setup_file = r"C:\Git\opensim_tutorial\tutorials\repeated_sprinting\Simulations\009\pre\ceinms\calibration\calibrationSetup.xml"
+    run_calibration(xml_setup_file)
